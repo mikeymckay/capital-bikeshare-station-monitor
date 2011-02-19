2b9a6f8ba4105aca55cd4dc8f08f639ffbe169bc
crazy <crazy@mikeymckay.xen.prgmr.com>
2011-02-19 07:59:12 +0000

    Switched to couch.one hosting

--- updateBikeInfo.rb
+++ updateBikeInfo.rb
@@ -5,7 +5,7 @@ require 'couchrest'
 
 current_time = Time.now.to_i
 
-CouchRest.database!("http://127.0.0.1:5984/capital-bikeshare").bulk_save(
+CouchRest.database!("http://mikeymckay.couchone.com/capital-bikeshare").bulk_save(
   Nokogiri::XML(open('http://www.capitalbikeshare.com/stations/bikeStations.xml')).css("stations station name").map{|station|
     ["id","nbBikes","nbEmptyDocks"].inject({:timestamp => current_time}) do |result, property|
       result[property] = station.parent.css(property).first.content
