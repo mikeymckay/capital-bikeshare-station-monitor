816ab0c18fafc25b614753c1aa7049cc5e4ab620
Mike McKay <mikeymckay@gmail.com>
2011-02-28 15:46:41 -0500

    Added zooming graphs and period summaries

--- updateBikeInfo.rb
+++ updateBikeInfo.rb
@@ -3,9 +3,12 @@ require 'nokogiri'
 require 'open-uri'
 require 'couchrest'
 
+#database_uri = "http://mikeymckay.couchone.com/capital-bikeshare"
+database_uri = "http://localhost:5984/test"
+
 current_time = Time.now.to_i
 
-CouchRest.database!("http://mikeymckay.couchone.com/capital-bikeshare").bulk_save(
+CouchRest.database!(database_uri).bulk_save(
   Nokogiri::XML(open('http://www.capitalbikeshare.com/stations/bikeStations.xml')).css("stations station name").map{|station|
     ["id","nbBikes","nbEmptyDocks"].inject({:timestamp => current_time}) do |result, property|
       result[property] = station.parent.css(property).first.content
